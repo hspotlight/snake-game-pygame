6ed59e6f29e8a68aa0039d2e873e60a0b0bdf89f
Pakawat Anekwiroj <005666@cdg.co.th>
2019-03-03 20:55:51 +0700

    Grid - handle render grid cell by fill with the color

--- main.py
+++ main.py
@@ -1,33 +1,170 @@
 import pygame
+from random import shuffle
+import os
+
 import color
 import snake
 
-max_width = 1000    
-max_height = 800
-screen = pygame.display.set_mode((max_width, max_height))
-pygame.display.set_caption('Hong Snake Ja')
+# config
+background_color = color.black
+edge_color = (0, 153, 153)
+snake_head = color.red
+snake_body = color.green
+snake_tail = color.blue
+food_color = (111, 111, 111)
+obstacle_color = (255, 115, 0)
 
-running = True
-clock = pygame.time.Clock()
-frame_per_second = 60
+edge_margin = 10
+edge_size = 10
+screen_margin = edge_margin + edge_size
+
 
 # block
-block_width = 20
-block_height = 20
+block_width = 36
+block_height = 36
+col_size = 30
+row_size = 20
+screen_width = block_width * col_size
+screen_height = block_height * row_size
+screen = pygame.display.set_mode((screen_width + edge_margin * 4, screen_height + edge_margin * 4))
+pygame.display.set_caption('Hong Snake Project')
 
-initial_length = 5
-initial_position = [10, 10]
+all_cell = []
+for x in range(0, col_size):
+    for y in range(0, row_size):
+        all_cell.append([x, y])
+
+# food
+food = None
+
+def handle_render_screen(screen_width, screen_height):
+    screen.fill(color.black)
+    pygame.draw.rect(screen, edge_color, (edge_margin, edge_margin, screen_width + edge_margin * 2, screen_height + edge_margin * 2))
+    pygame.draw.rect(screen, background_color, (edge_margin + edge_size, edge_margin + edge_size, screen_width, screen_height))
+
+
+def handle_render_snake(moving_parts):
+    # update head
+    handle_assign_cell(moving_parts[0], 1)
+    handle_draw_cell(snake_head, moving_parts[0][0], moving_parts[0][1])
+    handle_draw_cell(snake_body, moving_parts[1][0], moving_parts[1][1])
+
+    # update tail
+    if len(moving_parts) > 2:
+        handle_draw_cell(snake_tail, moving_parts[2][0], moving_parts[2][1])
+        handle_assign_cell(moving_parts[3], 0)
+        handle_draw_cell(background_color, moving_parts[3][0], moving_parts[3][1])
+
+
+def handle_render_background(all_cell):
+    for cell in all_cell:
+        handle_blit_cell(wall, cell[0], cell[1])
+
+def handle_render_obstacle(obstacles):
+    for obstacle in obstacles:
+        # obstacle in color
+        handle_draw_cell(obstacle_color, obstacle[0], obstacle[1])
+
+
+def handle_render_food():
+    temp = all_cell.copy()
+    shuffle(temp)
+    point = None
+    for cell in temp:
+        if (get_cell(cell) == 0):
+            point = cell
+
+    if point != None:
+        food = point
+        handle_assign_cell(point, 3)
+        handle_draw_cell(food_color, point[0], point[1])
+
+
+# draw image in the specify XY coordinates
+def handle_blit_cell(image, cell_col, cell_row):
+    screen.blit(image, 
+        (cell_col * block_width + screen_margin,
+        cell_row * block_height + screen_margin))
+
+
+def handle_draw_cell(color, cell_col, cell_row):
+    pygame.draw.rect(screen, color, (
+        cell_col * block_width + screen_margin,
+        cell_row * block_height + screen_margin,
+        block_width, 
+        block_height))
+
+
+def handle_assign_cell(point, value):
+    x = point[0]
+    y = point[1]
+    grid_map[y][x] = value
+
+
+def check_in_range(point, col_size, row_size):
+    return (0 <= point[0] and point[0] < col_size) and (0 <= point[1] and point[1] < row_size)
+
+
+def handle_object_move_out_screen(point, col_size, row_size):
+    x, y = point
+    if x < 0:
+        x = col_size - 1
+    elif x >= col_size:
+        x = 0
+    elif y < 0:
+        y = row_size - 1
+    elif y >= row_size:
+        y = 0
+
+    return [x, y]
+
+
+def get_cell(point):
+    return grid_map[point[1]][point[0]]
+
+
+# handle grid
+# each cell represents a object in the grid map
+# 0 = empty
+# 1 = snake
+# 2 = obstacle
+# 3 = food
+grid_map = [[0 for col in range(0, col_size)] for row in range(0, row_size)]
+
+# construct wall - array of position (x, y) = (col, row)
+obstacles = []
+for x in range(0, col_size):
+    for y in range(0, row_size):
+        if (x == 0 or x == col_size-1) or (y == 0 or y == row_size-1):
+            # all bourdary cell
+            # obstacles.append([x, y])
+            # handle_assign_cell([x, y], 2)
+
+            # make some hole for moving through the screen
+            if (y < row_size/2 -1 or y > row_size/2 + 1):
+                obstacles.append([x, y])
+                handle_assign_cell([x, y], 2)
+
+
+# create a snake
+initial_length = 15
+initial_position = [int(col_size/2), int(row_size/2)]
 moving_direction = 'D'
 my_snake = snake.Snake(moving_direction, initial_position, initial_length)
 
-screen.fill(color.white)
-count = 1
+
+handle_render_screen(screen_width, screen_height)
+handle_render_obstacle(obstacles)
+handle_render_food()
+running = True
 while running:
 
+    # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # handle event from user
         if event.type == pygame.KEYDOWN:
             if (event.key == pygame.K_UP or event.key == ord('w')) and moving_direction != 'S':
                 moving_direction = 'W'
@@ -38,27 +175,37 @@ while running:
             elif (event.key == pygame.K_RIGHT or event.key == ord('d')) and moving_direction != 'A':
                 moving_direction = 'D'
 
-
     ## moving snake
     my_snake.set_moving_direction(moving_direction)
 
-    moving_parts = my_snake.move()
+    # peak next position
+    snake_head_position = my_snake.get_next_head_position()
 
-    # next_head
-    pygame.draw.rect(screen, color.red, (moving_parts[0][0] * block_width, moving_parts[0][1] * block_height, block_width, block_height))
+    # if snake move out of screen, it will be placed in the other side of the screen
+    if not check_in_range(snake_head_position, col_size, row_size):
+        snake_head_position = handle_object_move_out_screen(snake_head_position, col_size, row_size)
 
-    # prev_head
-    pygame.draw.rect(screen, color.green, (moving_parts[1][0] * block_width, moving_parts[1][1] * block_height, block_width, block_height))
+    cell = get_cell(snake_head_position)
 
-    # update tail
-    if len(moving_parts) > 2:
-        # next_tail
-        pygame.draw.rect(screen, color.blue, (moving_parts[2][0] * block_width, moving_parts[2][1] * block_height, block_width, block_height))
+    remove_tail=True
+    drop_food=False
+    if cell == 1: # snake itself
+        running = False
+    elif cell == 2: # obstacle
+        running = False
+    elif cell == 3: # food
+        # create new food
+        drop_food = True
+        remove_tail = False
 
-        # prev_tail
-        # same color as background
-        pygame.draw.rect(screen, color.white, (moving_parts[3][0] * block_width, moving_parts[3][1] * block_height, block_width, block_height))
+
+    moving_parts = my_snake.move(snake_head_position, remove_tail)
+    handle_render_snake(moving_parts)
+
+    if drop_food:
+        handle_render_food()
 
 
+    # update map
     pygame.display.update()
     pygame.time.delay(70)
--- snake.py
+++ snake.py
@@ -1,9 +1,13 @@
 class Snake:
+    # initial the properties of a snake
     def __init__(self, moving_direction, point, initial_length=5):
         self.moving_direction = moving_direction
         self.location = [point]
         self.initial_length = initial_length
 
+
+    # get the next position of the snake according to its moving direction
+    # note that this function is only used for peak where the snake is going to
     def get_next_head_position(self):
 
         # get current head position
@@ -23,17 +27,18 @@ class Snake:
     def get_current_head_position(self):
         return self.location[0].copy()
 
+    # set the moving direction of the snake from an event triggered by user or other object
     def set_moving_direction(self, moving_direction):
         self.moving_direction = moving_direction
 
     # return the moving parts of the snake includes next head, current head, and previous tail
     # output = (head_next, head_prev, tail_next?, tail_prev?)
-    def move(self):
-        head_next = self.get_next_head_position()
+    def move(self, next_head_position, remove_tail=True):
+        head_next = next_head_position
         head_prev = self.get_current_head_position()
 
         output = [head_next, head_prev]
-        if len(self.location) >= self.initial_length and len(self.location) >= 2:
+        if remove_tail and len(self.location) >= self.initial_length and len(self.location) >= 2:
             tail_prev = self.location.pop()
             tail_next = self.location[-1]
             output.append(tail_next)
